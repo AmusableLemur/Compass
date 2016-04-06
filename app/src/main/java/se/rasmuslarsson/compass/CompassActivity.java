aea7be9a1ceaa64d4c6050221e2bc81292414946
Rasmus Larsson <rasmus@rasmuslarsson.se>
2016-04-06 21:12:55 +0200

    New implementation

--- app/src/main/java/se/rasmuslarsson/compass/CompassActivity.java
+++ app/src/main/java/se/rasmuslarsson/compass/CompassActivity.java
@@ -6,7 +6,6 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
@@ -16,19 +15,16 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
 
     private ImageView image;
 
+    private float azimuth = 0f;
+    private float azimuthInDegrees = 0f;
     private float currentDegree = 0f;
 
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
     private Sensor mMagnetometer;
 
-    private float[] mLastAccelerometer = new float[3];
-    private float[] mLastMagnetometer = new float[3];
-    private boolean mLastAccelerometerSet = false;
-    private boolean mLastMagnetometerSet = false;
-
-    private float[] mR = new float[9];
-    private float[] mOrientation = new float[3];
+    private float[] mGravity;
+    private float[] mGeomagnetic;
 
     TextView heading;
 
@@ -47,8 +43,6 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
     @Override
     protected void onResume() {
         super.onResume();
-        mLastAccelerometerSet = false;
-        mLastMagnetometerSet = false;
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
@@ -61,34 +55,37 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor == mAccelerometer) {
-            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
-            mLastAccelerometerSet = true;
-        } else if (event.sensor == mMagnetometer) {
-            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
-            mLastMagnetometerSet = true;
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            mGravity = event.values;
+
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            mGeomagnetic = event.values;
+
+        if (mGravity != null && mGeomagnetic != null) {
+            float R[] = new float[9];
+            float I[] = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+            if (success) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                azimuthInDegrees = (float)Math.toDegrees(orientation[0]);
+            }
         }
 
-        if (mLastAccelerometerSet && mLastMagnetometerSet) {
-            SensorManager.getRotationMatrix(mR, null, mLastAccelerometer, mLastMagnetometer);
-            SensorManager.getOrientation(mR, mOrientation);
-            float azimuthInRadians = mOrientation[0];
-            float azimuthInDegress = (float)(Math.toDegrees(azimuthInRadians) + 360) % 360;
-            RotateAnimation ra = new RotateAnimation(
-                    currentDegree,
-                    -azimuthInDegress,
-                    Animation.RELATIVE_TO_SELF, 0.5f,
-                    Animation.RELATIVE_TO_SELF,
-                    0.5f);
+        RotateAnimation ra = new RotateAnimation(
+                currentDegree,
+                -azimuthInDegrees,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f);
 
-            ra.setDuration(210);
-            ra.setFillAfter(true);
+        ra.setDuration(210);
+        ra.setFillAfter(true);
 
-            heading.setText("Heading: " + (int)Math.abs(currentDegree) + " degrees");
+        heading.setText("Heading: " + (int)Math.abs(currentDegree) + " degrees");
 
-            image.startAnimation(ra);
-            currentDegree = -azimuthInDegress;
-        }
+        image.startAnimation(ra);
+        currentDegree = -azimuthInDegrees;
     }
 
     @Override
